f713dccfe447308dc9507db536a2206bdb59be8c
ehsan42324232 <68990539+ehsan42324232@users.noreply.github.com>
2025-07-16 20:17:54 +0330

    Fix import path from .models to ..models

--- shop/serializers/__init__.py
+++ shop/serializers/__init__.py
@@ -1,7 +1,7 @@
 
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import Store, Product, Category, Media, Comment, Rating, Basket, Order, OrderItem
+from ..models import Store, Product, Category, Media, Comment, Rating, Basket, Order, OrderItem
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
